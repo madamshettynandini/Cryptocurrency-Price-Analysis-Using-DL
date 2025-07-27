010898949993e6e1011aadebee151048e6a9dc65
madamshettynandini <133802436+madamshettynandini@users.noreply.github.com>
2025-07-27 12:27:00 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from django.db import models
+import datetime
 from django.utils import timezone
-
-
 # Create your models here.
-class BitAgentRegisterModel(models.Model):
-    id = models.AutoField(primary_key=True)
+
+class BitUserRegisterModel(models.Model):
+    id = models.AutoField(primary_key = True)
     email = models.CharField(max_length=100, unique=True)
     pswd = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
@@ -12,46 +12,39 @@ class BitAgentRegisterModel(models.Model):
     pan = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    cryptcurrency = models.CharField(max_length=100)
-    status = models.CharField(max_length=100, default='waiting')
-    authkey = models.CharField(max_length=100, default='waiting')
-    # cdate = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100,default='waiting')
+    authkey = models.CharField(max_length=100,default='waiting')
+    #cdate = models.DateTimeField(auto_now_add=True)
     cdate = models.DateTimeField()
-
     def __str__(self):
         return self.email
-
     class Meta:
-        db_table = 'agentregister'
+        db_table = 'userregister'
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
         if not self.id:
             self.cdate = timezone.now()
-        return super(BitAgentRegisterModel, self).save(*args, **kwargs)
-
-
-class AgentHadCrypto(models.Model):
+        return super(BitUserRegisterModel, self).save(*args, **kwargs)
+class CustomerHadCoins(models.Model):
     id = models.AutoField(primary_key=True)
     currencyName = models.CharField(max_length=100)
-    useremail = models.CharField(max_length=100)
+    customeremail = models.CharField(max_length=100)
     quantity = models.IntegerField()
 
     def __str__(self):
-        return self.useremail
-
+        return self.customeremail
     class Meta:
-        db_table = "agentscryptoquantity"
-        unique_together = ('currencyName', 'useremail',)
-
-
-class AgentBuyCryptoModel(models.Model):
+        db_table = "CustomerContainCoins"
+        unique_together = ('currencyName', 'customeremail',)
+class UserBuyingCryptoModel(models.Model):
     id = models.AutoField(primary_key=True)
-    agentName = models.CharField(max_length=100)
-    agentemail = models.CharField(max_length=100)
+    customername = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
     currencyname = models.CharField(max_length=100)
-    currentprice = models.FloatField()
     quantity = models.IntegerField()
+    agentemail = models.CharField(max_length=100)
+    singlecoingamount = models.FloatField()
     payableammount = models.FloatField()
     cardnumber = models.CharField(max_length=100)
     nameoncard = models.CharField(max_length=100)
@@ -62,6 +55,18 @@ class AgentBuyCryptoModel(models.Model):
     def __str__(self):
         return self.id
     class Meta:
-        db_table = 'AgentBuyedTransactions'
+        db_table = 'UserBuyingCryptoModel'
 
+class BlockChainLedger(models.Model):
+    id = models.AutoField(primary_key=True)
+    customeremail = models.CharField(max_length=100)
+    agentemail = models.CharField(max_length=100)
+    currencyname = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    paidammout = models.FloatField()
+    blockchainmoney = models.FloatField()
 
+    def __str__(self):
+        return self.id
+    class Meta:
+        db_table = "BlockChainLedger"
